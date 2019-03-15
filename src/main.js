87f1955c5ebb24d122bea83ea3652107351608a9
ray.cen <ray.cen@ef.com>
2019-03-15 14:02:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -29,13 +29,10 @@ function paintMap() {
 	docBody = '';
 	doctitle = document.getElementById('text-input-id-11').value;
 	docContent = document.getElementById('text-input-id-12').value;
-	docBody = '<link rel="stylesheet" href="https://cdn.ef.design/libs/gud-css/0.0.1/css/main.min.css" crossorigin="anonymous">';
-	docBody += '<div class="ef-card--content -outline"> <a href="#" class="ef-card--content__img" tabindex="-1" ><img border="0" src="https://placeimg.com/640/480/arch/1" /></a>';
-	docBody += '<div class="ef-card--content__inner"><div class="ef-card--content__content"> <h4 class="ef-card--content__title">'; 
-	docBody += doctitle; 
-	docBody += '</h4><div class="ef-card--content__text"><p>';
-	docBody += docContent; 
-	docBody += '</p><a href="#" class="ef-card--content__link">Link to page</a> </div> </div></div>';
+	docBody = '<html><link rel="stylesheet" href="https://cdn.ef.design/libs/gud-css/0.0.1/css/main.min.css" crossorigin="anonymous"><div class="ef-card--content"><a href="#" class="ef-card--content__img" tabindex="-1" ><img border="0" src="https://placeimg.com/640/480/arch/1" /></a><div class="ef-card--content__inner"><div class="ef-card--content__content"><h4 class="ef-card--content__title">{1}</h4><div class="ef-card--content__text"> <p>{2}</p><a href="#" class="ef-card--content__link">Link to page</a></div></div></div></div></html>';
+	docBody = docBody.replace("{1}",doctitle);
+	docBody = docBody.replace("{2}",docContent);
+
 	console.log(docBody);
 	sdk.setContent(docBody);
 	sdk.setData({
